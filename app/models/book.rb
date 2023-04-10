c7a519781a06c531ede1c3377eac8546b1e8bbf5
MayuYazawa <121260144+MayuYazawa@users.noreply.github.com>
2023-04-10 06:35:50 +0000

    コントローラー、モデル追加

---
+++ app/models/book.rb
@@ -0,0 +1,63 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_tags, dependent: :destroy
+  has_many :tags, through: :book_tags
+  has_many :view_counts, dependent: :destroy
+  has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
+  
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } # 前週
+  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) } # 2日前
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) } # 3日前
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) } # 4日前
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) } # 5日前
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) } # 6日前
+  
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+  
+  def save_tags(savebook_tags)
+    # 現在のユーザーの持っているskillを引っ張ってきている
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    # 今bookが持っているタグと今回保存されたものの差をすでにあるタグとする。古いタグは消す。
+    old_tags = current_tags - savebook_tags
+    # 今回保存されたものと現在の差を新しいタグとする。新しいタグは保存
+    new_tags = savebook_tags - current_tags
+		
+    # Destroy old taggings:
+    old_tags.each do |old_name|
+      self.tags.delete Tag.find_by(name:old_name)
+    end
+		
+    # Create new taggings:
+    new_tags.each do |new_name|
+      book_tag = Tag.find_or_create_by(name:new_name)
+      # 配列に保存
+      self.tags << book_tag
+    end
+  end
+	
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  User.create(
+    name: "testuser#{n+1}",
+    email: "testuser#{n+1}@test.com",
+    password: "testuser#{n+1}",
+  )
+  Book.create(
+    title: "testbook#{n+1}",
+    body: "testbook#{n+1}",
+    user_id: n+1,
+    created_at: Time.current - (n+1).to_i.day
+  )
+end 
+
+Group.create(
+  name: "Test Group",
+  introduction: "Test Event Body",
+  owner_id: 1
+)
+
+# Userそれぞれに対して、10レコードずつBookを作成
+# 現在時刻から 0 ~ 15 までのランダムな数字を引いた時刻でBookを作成
+
+User.all.each do |user|
+  10.times do |n|
+    Book.create(
+      title: "testbook#{n+1}",
+      body: "testbook#{n+1}",
+      user_id: user.id,
+      created_at: Time.current - rand(15).day
+    )
+  end
+end
+
+i = User.first.id
+
+# Bookそれぞれに対してUser(1 ~ i)とのFavoriteを作成する。
+# Bookのidが大きくなるほど紐つけられるFavoriteモデルの数が多くなる。
+# Favoriteの作成時刻に違いを出すために「現在時刻 - i」 の時刻で作成している。
+
+i = User.first.id # usersテーブル内の最初のレコードのidを取得
+
+Book.all.each do |book|
+  for user_id in 1..i do
+    Favorite.create(
+      user_id: user_id,
+      book_id: book.id,
+      created_at: Time.current - i.day
+    )
+  end
+  i+=1
+end
