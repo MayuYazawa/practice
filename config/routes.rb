d43b940127778a7ee1340a371bcd6ef98918c9e9
MayuYazawa <121260144+MayuYazawa@users.noreply.github.com>
2023-04-13 04:56:51 +0000

    viewとroures

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+    get "daily_posts" => "users#daily_posts"
+  end
+  
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
+    resource :group_users, only: [:create, :destroy]
+    resources :event_notices, only: [:new, :create]
+    get "event_notices" => "event_notices#sent"
+  end
+  
+  get '/search', to: 'searches#search'
+  
+  resources :chats, only: [:show, :create]
 end
